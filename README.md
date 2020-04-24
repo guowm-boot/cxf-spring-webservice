162c9b0cf9c6349cfc94e1c7cb41ea6f00a1dd76
guowm-boot <beocenter@163.com>
2020-04-24 15:09:32 +0800

    readMe

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cxf-spring-webservice
 在spring中使用CXF框架，搭建webservice的服务端和客户端，使用soap协议
+
+
+**注意事项**：
+1、注意CXF与spring的版本配合，否则将启动失败
--- src/main/java/com/solaris/cxf_spring_webservice/CommonService.java
+++ src/main/java/com/solaris/cxf_spring_webservice/CommonService.java
@@ -5,9 +5,6 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
-//        (name = "CommonService", // 暴露服务名称
-//        targetNamespace = "http://webservice.lli.com/"// 命名空间,一般是接口的包名倒序
-//)
 public interface CommonService {
     @WebMethod
     String sayHello(@WebParam(name = "param") String param);
